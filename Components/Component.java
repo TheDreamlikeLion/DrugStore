259c09bf54efb87b37e5fbe1828956389288d452
TheDreamlikeLion <144340011+TheDreamlikeLion@users.noreply.github.com>
2024-02-05 14:44:39 +0300

    Update Component.java

--- Components/Component.java
+++ Components/Component.java
@@ -1,1 +1,34 @@
+package DrugStore.Components;
 
+public abstract class Component implements Comparable<Component> {
+    private String title;
+    private double weight;
+    private int power;
+
+    public Component(String title, double weight, int power) {
+        this.title = title;
+        this.weight = weight;
+        this.power = power;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Title: %s, power: %s\n", this.title, this.power);
+    }
+
+    @Override
+    public int compareTo(Component component) {
+//        if (this.power > component.power) return 1;
+//        else if (this.power < component.power) return -1;
+//        else return 0;
+        return Integer.compare(this.power, component.power);
+    }
+}
